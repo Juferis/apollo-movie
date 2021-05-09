a084f0efe24ab11e0b346624f0bbf87c64e44fc8
Juferis <juferis@naver.com>
2021-05-09 11:48:55 +0900

    Router and reset css

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
 
 function App() {
-  return <div className="App"></div>;
+  // exact : 주어진 경로가 정확해야지만 페이지를 보여준다.
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/:id" component={Detail} />
+    </Router>
+  );
 }
 
 export default App;
